7bfee57f3c3c302482fa99e7f167fc147d9f15b9
munair <munair@qe.local>
2016-05-04 21:50:15 +0900

    remove absolute

--- templates/banner.php
+++ templates/banner.php
@@ -6,7 +6,7 @@
 		<li></li>
 	</ul>
 	<div class="v-center">
-		<div class="slide what-down"><p>We are an <span style="font-weight: 700;">international organization</span></p></div>
-		<div class="slide what-up"><p>based in <span style="font-weight: 700;">Seoul, South Korea</span>.</p><div class="yellow-button">Usage</div></div>
+                <div class="slide what-down"><p>We are an <span style="font-weight: 700;">international organization</span></p></div>
+                <div class="slide what-up"><p>based in <span style="font-weight: 700;">Seoul, South Korea</span>.</p><div class="yellow-button">Usage</div></div>
 	</div>
 </section>
