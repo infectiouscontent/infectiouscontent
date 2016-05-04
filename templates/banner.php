29e5191bd458ea12ee69e0b6e089fb9cf1ba01de
munair <munair@qe.local>
2016-05-04 21:21:15 +0900

    fix ribbon header weight and animations on banner

--- templates/banner.php
+++ templates/banner.php
@@ -5,13 +5,8 @@
 		<li></li>
 		<li></li>
 	</ul>
-	<div class="slide what-down"><p>We are an <h3>international organization</h3></p></div>
-	<div class="slide what-up"><p>based in <h3>Seoul, South Korea</h3>.</p><div class="yellow-button">Usage</div></div>
 	<div class="v-center">
-		<a onmousedown="tick()" onmouseover="pop()" href="#ribbon">
-			<img class="scroll-down" src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/faded-handshaker-logo-1000x1000px.png" />
-		</a>
-		<h1><?php bloginfo('name'); ?></h1> 
-		<p><?php bloginfo('description'); ?></p>	
+		<div class="slide what-down"><p>We are an <h3>international organization</h3></p></div>
+		<div class="slide what-up"><p>based in <h3>Seoul, South Korea</h3>.</p><div class="yellow-button">Usage</div></div>
 	</div>
 </section>
