34b59597afabb3e086e5b4a9b5c1e3529463d250
munair <munair@qe.local>
2016-05-05 15:46:32 +0900

    remove unnecessary css. make animation name more transparent. make sure that position relative is in the right place.

--- templates/banner.php
+++ templates/banner.php
@@ -6,10 +6,10 @@
 		<li></li>
 	</ul>
 	<div class="v-center">
-                <div class="slide what-down">
+                <div class="slide move-down">
 			<p>We are an <span style="font-weight: 700;">international organization</span></p>
 		</div>
-                <div class="slide what-up">
+                <div class="slide move-up">
 			<p>based in <span style="font-weight: 700;">Seoul, South Korea</span>. 
 			<div class="yellow-button"> read more <i class="fa fa-chevron-right" style="font-size:15px;"></i></div>
 			</p>
