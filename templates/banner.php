19f0ae7c99d8bf3a2574fc3d37e7d487750bad27
munair <munair@qe.local>
2016-05-05 15:28:17 +0900

    rollback

--- templates/banner.php
+++ templates/banner.php
@@ -6,7 +6,13 @@
 		<li></li>
 	</ul>
 	<div class="v-center">
-                <div class="slide what-down"><p>We are an <span style="font-weight: 700;">international organization</span></p></div>
-                <div class="slide what-up"><p>based in <span style="font-weight: 700;">Seoul, South Korea</span>. <div class="yellow-button"> read more <i class="pull-right-icon fa fa-chevron-right" style="font-size:15px;"></i></div></div></p>
+                <div class="slide what-down">
+			<p>We are an <span style="font-weight: 700;">international organization</span></p>
+		</div>
+                <div class="slide what-up">
+			<p>based in <span style="font-weight: 700;">Seoul, South Korea</span>. 
+			<div class="yellow-button"> read more <i class="pull-right-icon fa fa-chevron-right" style="font-size:15px;"></i></div>
+			</p>
+		</div>
 	</div>
 </section>
