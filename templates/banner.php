22fef18404c84dcf716d448956d3d652f8243863
munair <munair@qe.local>
2016-05-06 15:59:39 +0900

    there is a problem with the text elements

--- templates/banner.php
+++ templates/banner.php
@@ -7,7 +7,7 @@
 	</ul>
 	<div class="v-center">
                 <div class="slide vflip">
-			<p> We are an <span style="font-weight: 900;">international organization</span> based in <span style="font-weight: 900;">Seoul, South Korea</span>. </p>
+			<p> We develop <span style="font-weight: 900;">vaccines</span> for <span style="font-weight: 900;">global health</span>. </p>
 		</div>
                 <div class="slide vflop">
 			<div class="yellow-button"> <p> find us <i class="glyphicon glyphicon-chevron-right" style="font-size:15px;"></i> </p> </div>
