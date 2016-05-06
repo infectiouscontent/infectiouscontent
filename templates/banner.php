7a553ee9cbaf5fa074efe5672d993d17c69ed179
munair <munair@qe.local>
2016-05-06 14:54:45 +0900

    implement fake transition

--- templates/banner.php
+++ templates/banner.php
@@ -6,6 +6,12 @@
 		<li></li>
 	</ul>
 	<div class="v-center">
+                <div class="slide vflip">
+			<p> We are an <span style="font-weight: 900;">international organization</span> based in <span style="font-weight: 900;">Seoul, South Korea</span>. </p>
+		</div>
+                <div class="slide vflop">
+			<div class="yellow-button"> <p> find us <i class="glyphicon glyphicon-chevron-right" style="font-size:15px;"></i> </p> </div>
+		</div>
                 <div class="slide down">
 			<p> We are an <span style="font-weight: 900;">international organization</span> based in <span style="font-weight: 900;">Seoul, South Korea</span>. </p>
 		</div>
