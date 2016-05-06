c988c726b38985966a0f12630ed175a71d6992d2
munair <munair@qe.local>
2016-05-06 18:29:12 +0900

    use vflop class

--- templates/banner.php
+++ templates/banner.php
@@ -6,10 +6,10 @@
 		<li></li>
 	</ul>
 	<div class="v-center">
-                <div class="slide flop">
+                <div class="slide vflop">
 			<p> We are an <span style="font-weight: 900;">international organization</span> based in <span style="font-weight: 900;">Seoul, South Korea</span>. </p>
 		</div>
-                <div class="slide flop">
+                <div class="slide vflop">
 			<div class="yellow-button"> <p> find us <i class="glyphicon glyphicon-chevron-right" style="font-size:15px;"></i> </p> </div>
 		</div>
 	</div>
