54ddcff5e34b3e4037955ea8bf326563ebc72ac5
munair <munair@qe.local>
2016-05-06 18:22:25 +0900

    testing flop

--- templates/banner.php
+++ templates/banner.php
@@ -6,10 +6,10 @@
 		<li></li>
 	</ul>
 	<div class="v-center">
-                <div class="slide down">
+                <div class="slide flop">
 			<p> We are an <span style="font-weight: 900;">international organization</span> based in <span style="font-weight: 900;">Seoul, South Korea</span>. </p>
 		</div>
-                <div class="slide up">
+                <div class="slide flop">
 			<div class="yellow-button"> <p> find us <i class="glyphicon glyphicon-chevron-right" style="font-size:15px;"></i> </p> </div>
 		</div>
 	</div>
