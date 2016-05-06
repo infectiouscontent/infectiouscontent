de0d2f4edd3ee1f9e171ceb2b21f64aebb614fdc
munair <munair@qe.local>
2016-05-06 14:00:25 +0900

    implement banner rotation over 33 seconds... 2 second delay for the first 9 second slide means that the second slide should start after 11 seconds.

--- templates/banner.php
+++ templates/banner.php
@@ -6,12 +6,23 @@
 		<li></li>
 	</ul>
 	<div class="v-center">
-                <div class="slide move-down">
-			<p>We are an <span style="font-weight: 900;">international organization</span> based in <span style="font-weight: 900;">Seoul, South Korea</span>. 
+                <div class="slide flip">
+			<p> We <span style="font-weight: 900;">prevent</span> death and suffering from <span style="font-weight: 900;">infectious disease</span>. </p>
 		</div>
-                <div class="slide move-up">
-			<div class="yellow-button"> <p> more <i class="glyphicon glyphicon-chevron-right" style="font-size:15px;"></i> </p> </div>
-			</p>
+                <div class="slide flop">
+			<div class="yellow-button"> <p> join us <i class="glyphicon glyphicon-chevron-right" style="font-size:15px;"></i> </p> </div>
+		</div>
+                <div class="slide left">
+			<p> We develop <span style="font-weight: 900;">vaccines</span> for <span style="font-weight: 900;">global health</span>. </p>
+		</div>
+                <div class="slide right">
+			<div class="yellow-button"> <p> help us <i class="glyphicon glyphicon-chevron-right" style="font-size:15px;"></i> </p> </div>
+		</div>
+                <div class="slide down">
+			<p> We are an <span style="font-weight: 900;">international organization</span> based in <span style="font-weight: 900;">Seoul, South Korea</span>. </p>
+		</div>
+                <div class="slide up">
+			<div class="yellow-button"> <p> find us <i class="glyphicon glyphicon-chevron-right" style="font-size:15px;"></i> </p> </div>
 		</div>
 	</div>
 </section>
