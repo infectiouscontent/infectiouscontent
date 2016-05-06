6bbb8e721acc3c5991b681a84f480e65a84a3c67
munair <munair@qe.local>
2016-05-06 14:20:07 +0900

    multiple v-senters

--- templates/banner.php
+++ templates/banner.php
@@ -12,12 +12,16 @@
                 <div class="slide flop">
 			<div class="yellow-button"> <p> join us <i class="glyphicon glyphicon-chevron-right" style="font-size:15px;"></i> </p> </div>
 		</div>
+	</div>
+	<div class="v-center">
                 <div class="slide left">
 			<p> We develop <span style="font-weight: 900;">vaccines</span> for <span style="font-weight: 900;">global health</span>. </p>
 		</div>
                 <div class="slide right">
 			<div class="yellow-button"> <p> help us <i class="glyphicon glyphicon-chevron-right" style="font-size:15px;"></i> </p> </div>
 		</div>
+	</div>
+	<div class="v-center">
                 <div class="slide down">
 			<p> We are an <span style="font-weight: 900;">international organization</span> based in <span style="font-weight: 900;">Seoul, South Korea</span>. </p>
 		</div>
