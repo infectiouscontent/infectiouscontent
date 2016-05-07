46bf27769ccf4fbadbae4a872ab84d029908a473
munair <munair@qe.local>
2016-05-07 16:28:57 +0900

    re-write

--- templates/banner.php
+++ templates/banner.php
@@ -5,32 +5,32 @@
 		<li></li>
 		<li></li>
 	</ul>
-	<div class="stack layer-3">
-		<div class="centering">
-                	<div class="slide push">
+	<div class="stack">
+		<div class="layer-3">
+                	<div class="push">
 				<p> We develop <span style="font-weight: 900;">vaccines</span> for <span style="font-weight: 900;">global health</span>. </p>
 			</div>
-                	<div class="slide push">
+                	<div class="push">
 				<div class="yellow-button"> <p> join us <i class="glyphicon glyphicon-chevron-right" style="font-size:15px;"></i> </p> </div>
 			</div>
 		</div>
 	</div>
-	<div class="stack layer-2">
-		<div class="centering">
-                	<div class="slide left">
+	<div class="stack">
+		<div class="layer-2">
+                	<div class="left">
 				<p> We <span style="font-weight: 900;">collaborate globally</span> to fight <span style="font-weight: 900;">infectious disease</span>. </p>
 			</div>
-                	<div class="slide right">
+                	<div class="right">
 				<div class="yellow-button"> <p> help us <i class="glyphicon glyphicon-chevron-right" style="font-size:15px;"></i> </p> </div>
 			</div>
 		</div>
 	</div>
-	<div class="stack layer-1">
-		<div class="centering">
-                	<div class="slide down">
+	<div class="stack">
+		<div class="layer-1">
+                	<div class="down">
 				<p> We are an <span style="font-weight: 900;">international organization</span> based in <span style="font-weight: 900;">Seoul, South Korea</span>. </p>
 			</div>
-                	<div class="slide up">
+                	<div class="up">
 				<div class="yellow-button"> <p> find us <i class="glyphicon glyphicon-chevron-right" style="font-size:15px;"></i> </p> </div>
 			</div>
 		</div>
