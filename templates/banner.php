04487e7199e11298f136186c7e88a55488457a6a
munair <munair@qe.local>
2016-05-07 12:29:53 +0900

    add layer three

--- templates/banner.php
+++ templates/banner.php
@@ -6,10 +6,16 @@
 		<li></li>
 	</ul>
 	<div class="v-center">
-                <div class="layer-1 slide right">
+                <div class="layer-3 slide right">
+			<p> We develop <span style="font-weight: 900;">vaccines</span> for <span style="font-weight: 900;">global health</span>. </p>
+		</div>
+                <div class="layer-3 slide left">
+			<div class="yellow-button"> <p> join us <i class="glyphicon glyphicon-chevron-right" style="font-size:15px;"></i> </p> </div>
+		</div>
+                <div class="layer-1 slide down">
 			<p> We are an <span style="font-weight: 900;">international organization</span> based in <span style="font-weight: 900;">Seoul, South Korea</span>. </p>
 		</div>
-                <div class="layer-1 slide left">
+                <div class="layer-1 slide up">
 			<div class="yellow-button"> <p> find us <i class="glyphicon glyphicon-chevron-right" style="font-size:15px;"></i> </p> </div>
 		</div>
 	</div>
