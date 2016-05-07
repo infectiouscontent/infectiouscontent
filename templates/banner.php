64720951a4157a8bd7eb35933656276a95c648ea
munair <munair@qe.local>
2016-05-07 16:08:01 +0900

    actually... the html wasn't properly formatted. i should test again.

--- templates/banner.php
+++ templates/banner.php
@@ -6,31 +6,31 @@
 		<li></li>
 	</ul>
 	<div class="stack layer-3">
-		<div class="slide">
-                	<div class="push">
+		<div class="centering">
+                	<div class="slide push">
 				<p> We develop <span style="font-weight: 900;">vaccines</span> for <span style="font-weight: 900;">global health</span>. </p>
 			</div>
-                	<div class="push">
+                	<div class="slide push">
 				<div class="yellow-button"> <p> join us <i class="glyphicon glyphicon-chevron-right" style="font-size:15px;"></i> </p> </div>
 			</div>
 		</div>
 	</div>
 	<div class="stack layer-2">
-		<div class="slide">
-                	<div class="left">
+		<div class="centering">
+                	<div class="slide left">
 				<p> We <span style="font-weight: 900;">collaborate globally</span> to fight <span style="font-weight: 900;">infectious disease</span>. </p>
 			</div>
-                	<div class="right">
+                	<div class="slide right">
 				<div class="yellow-button"> <p> help us <i class="glyphicon glyphicon-chevron-right" style="font-size:15px;"></i> </p> </div>
 			</div>
 		</div>
 	</div>
 	<div class="stack layer-1">
-		<div class="slide">
-                	<div class="down">
+		<div class="centering">
+                	<div class="slide down">
 				<p> We are an <span style="font-weight: 900;">international organization</span> based in <span style="font-weight: 900;">Seoul, South Korea</span>. </p>
 			</div>
-                	<div class="up">
+                	<div class="slide up">
 				<div class="yellow-button"> <p> find us <i class="glyphicon glyphicon-chevron-right" style="font-size:15px;"></i> </p> </div>
 			</div>
 		</div>
