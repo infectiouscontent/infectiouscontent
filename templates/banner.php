e72e3e11699de98d83a8c67e189061a43a7a9a8f
munair <munair@qe.local>
2016-05-07 15:45:55 +0900

    implement the layers relative to the stack

--- templates/banner.php
+++ templates/banner.php
@@ -5,7 +5,8 @@
 		<li></li>
 		<li></li>
 	</ul>
-	<div class="stack"><div class="layer-3">
+	<div class="stack">
+		<div layer-3">
 		<div class="centering">
                 	<div class="slide push">
 				<p> We develop <span style="font-weight: 900;">vaccines</span> for <span style="font-weight: 900;">global health</span>. </p>
@@ -14,9 +15,8 @@
 				<div class="yellow-button"> <p> join us <i class="glyphicon glyphicon-chevron-right" style="font-size:15px;"></i> </p> </div>
 			</div>
 		</div>
-	</div>
-	</div>
-	<div class="stack"><div class="layer-2">
+		</div>
+		<div layer-2">
 		<div class="centering">
                 	<div class="slide left">
 				<p> We <span style="font-weight: 900;">collaborate globally</span> to fight <span style="font-weight: 900;">infectious disease</span>. </p>
@@ -25,9 +25,8 @@
 				<div class="yellow-button"> <p> help us <i class="glyphicon glyphicon-chevron-right" style="font-size:15px;"></i> </p> </div>
 			</div>
 		</div>
-	</div>
-	</div>
-	<div class="stack"><div class="layer-1">
+		</div>
+		<div layer-1">
 		<div class="centering">
                 	<div class="slide down">
 				<p> We are an <span style="font-weight: 900;">international organization</span> based in <span style="font-weight: 900;">Seoul, South Korea</span>. </p>
@@ -36,6 +35,6 @@
 				<div class="yellow-button"> <p> find us <i class="glyphicon glyphicon-chevron-right" style="font-size:15px;"></i> </p> </div>
 			</div>
 		</div>
-	</div>
+		</div>
 	</div>
 </section>
