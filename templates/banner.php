ac8b4a1642852546227f6d0850e4abe19ae7dc86
munair <munair@qe.local>
2016-05-07 13:07:16 +0900

    implement layering before v-center

--- templates/banner.php
+++ templates/banner.php
@@ -5,20 +5,34 @@
 		<li></li>
 		<li></li>
 	</ul>
-	<div class="v-center layer-3">
-                <div class="slide right">
-			<p> We develop <span style="font-weight: 900;">vaccines</span> for <span style="font-weight: 900;">global health</span>. </p>
-		</div>
-                <div class="slide left">
-			<div class="yellow-button"> <p> join us <i class="glyphicon glyphicon-chevron-right" style="font-size:15px;"></i> </p> </div>
+	<div class="layer-3">
+		<div class="banner-frame v-center">
+                	<div class="slide right">
+				<p> We develop <span style="font-weight: 900;">vaccines</span> for <span style="font-weight: 900;">global health</span>. </p>
+			</div>
+                	<div class="slide left">
+				<div class="yellow-button"> <p> join us <i class="glyphicon glyphicon-chevron-right" style="font-size:15px;"></i> </p> </div>
+			</div>
 		</div>
 	</div>
-	<div class="v-center layer-1">
-                <div class="slide down">
-			<p> We are an <span style="font-weight: 900;">international organization</span> based in <span style="font-weight: 900;">Seoul, South Korea</span>. </p>
+	<div class="layer-2">
+		<div class="banner-frame v-center">
+                	<div class="slide right">
+				<p> We <span style="font-weight: 900;">collaborate globally</span> to fight <span style="font-weight: 900;">infectious disease</span>. </p>
+			</div>
+                	<div class="slide left">
+				<div class="yellow-button"> <p> help us <i class="glyphicon glyphicon-chevron-right" style="font-size:15px;"></i> </p> </div>
+			</div>
 		</div>
-                <div class="slide up">
-			<div class="yellow-button"> <p> find us <i class="glyphicon glyphicon-chevron-right" style="font-size:15px;"></i> </p> </div>
+	</div>
+	<div class="layer-1">
+		<div class="banner-frame v-center">
+                	<div class="slide down">
+				<p> We are an <span style="font-weight: 900;">international organization</span> based in <span style="font-weight: 900;">Seoul, South Korea</span>. </p>
+			</div>
+                	<div class="slide up">
+				<div class="yellow-button"> <p> find us <i class="glyphicon glyphicon-chevron-right" style="font-size:15px;"></i> </p> </div>
+			</div>
 		</div>
 	</div>
 </section>
