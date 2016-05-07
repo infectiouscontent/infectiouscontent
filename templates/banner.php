0f9f7182765be170b63fd9eea389e537909eb2e2
munair <munair@qe.local>
2016-05-07 12:37:10 +0900

    putting the paragraphs on different layers is not working. let's try having the v-center element on three different layers.

--- templates/banner.php
+++ templates/banner.php
@@ -5,17 +5,19 @@
 		<li></li>
 		<li></li>
 	</ul>
-	<div class="v-center">
-                <div class="layer-3 slide right">
+	<div class="v-center layer-3">
+                <div class="slide right">
 			<p> We develop <span style="font-weight: 900;">vaccines</span> for <span style="font-weight: 900;">global health</span>. </p>
 		</div>
-                <div class="layer-3 slide left">
+                <div class="slide left">
 			<div class="yellow-button"> <p> join us <i class="glyphicon glyphicon-chevron-right" style="font-size:15px;"></i> </p> </div>
 		</div>
-                <div class="layer-1 slide down">
+	</div>
+	<div class="v-center layer-1">
+                <div class="slide down">
 			<p> We are an <span style="font-weight: 900;">international organization</span> based in <span style="font-weight: 900;">Seoul, South Korea</span>. </p>
 		</div>
-                <div class="layer-1 slide up">
+                <div class="slide up">
 			<div class="yellow-button"> <p> find us <i class="glyphicon glyphicon-chevron-right" style="font-size:15px;"></i> </p> </div>
 		</div>
 	</div>
