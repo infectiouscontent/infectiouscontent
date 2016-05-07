f8c15b254d6068477ce5b9eefaf6803435a2e72e
munair <munair@qe.local>
2016-05-07 15:29:52 +0900

    html was not properly formatted

--- templates/banner.php
+++ templates/banner.php
@@ -15,7 +15,7 @@
 			</div>
 		</div>
 	</div>
-	<div class="stack layer-3">
+	<div class="stack layer-2">
 		<div class="centering">
                 	<div class="slide left">
 				<p> We <span style="font-weight: 900;">collaborate globally</span> to fight <span style="font-weight: 900;">infectious disease</span>. </p>
@@ -25,7 +25,7 @@
 			</div>
 		</div>
 	</div>
-	<div class="stack layer-3">
+	<div class="stack layer-1">
 		<div class="centering">
                 	<div class="slide down">
 				<p> We are an <span style="font-weight: 900;">international organization</span> based in <span style="font-weight: 900;">Seoul, South Korea</span>. </p>
