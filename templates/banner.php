18f94246951109cb6f975ad57f0d3753399e5532
munair <munair@qe.local>
2016-05-07 13:32:20 +0900

    rollback

--- templates/banner.php
+++ templates/banner.php
@@ -17,10 +17,10 @@
 	</div>
 	<div class="layer-2">
 		<div class="banner-frame v-center">
-                	<div class="slide right">
+                	<div class="slide left">
 				<p> We <span style="font-weight: 900;">collaborate globally</span> to fight <span style="font-weight: 900;">infectious disease</span>. </p>
 			</div>
-                	<div class="slide left">
+                	<div class="slide right">
 				<div class="yellow-button"> <p> help us <i class="glyphicon glyphicon-chevron-right" style="font-size:15px;"></i> </p> </div>
 			</div>
 		</div>
