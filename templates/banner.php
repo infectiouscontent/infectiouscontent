28b38ebca08870d3d14d67ad47e30ceb5fd7eba1
munair <munair@qe.local>
2016-05-07 17:40:28 +0900

    needed to fix html

--- templates/banner.php
+++ templates/banner.php
@@ -8,29 +8,33 @@
 	<div class="stack-1">
 		<div class="centering">
 			<div class="statement">
-                	<div class="push">
-				<p> We develop <span style="font-weight: 900;">vaccines</span> for <span style="font-weight: 900;">global health</span>. </p>
-			</div>
-                	<div class="push">
-				<div class="yellow-button"> <p> join us <i class="glyphicon glyphicon-chevron-right" style="font-size:15px;"></i> </p> </div>
-			</div>
+                		<div class="push">
+					<p> We develop <span style="font-weight: 900;">vaccines</span> for <span style="font-weight: 900;">global health</span>. </p>
+				</div>
+        	        	<div class="push">
+					<div class="yellow-button"> <p> join us <i class="glyphicon glyphicon-chevron-right" style="font-size:15px;"></i> </p> </div>
+				</div>
 			</div>
 		</div>
 	</div>
 	<div class="stack-2">
-                	<div class="left">
-				<p> We <span style="font-weight: 900;">collaborate globally</span> to fight <span style="font-weight: 900;">infectious disease</span>. </p>
-			</div>
-                	<div class="right">
-				<div class="yellow-button"> <p> help us <i class="glyphicon glyphicon-chevron-right" style="font-size:15px;"></i> </p> </div>
+			<div class="statement">
+                		<div class="left">
+					<p> We <span style="font-weight: 900;">collaborate globally</span> to fight <span style="font-weight: 900;">infectious disease</span>. </p>
+				</div>
+                		<div class="right">
+					<div class="yellow-button"> <p> help us <i class="glyphicon glyphicon-chevron-right" style="font-size:15px;"></i> </p> </div>
+				</div>
 			</div>
 	</div>
 	<div class="stack-3">
-                	<div class="down">
-				<p> We are an <span style="font-weight: 900;">international organization</span> based in <span style="font-weight: 900;">Seoul, South Korea</span>. </p>
-			</div>
-                	<div class="up">
-				<div class="yellow-button"> <p> find us <i class="glyphicon glyphicon-chevron-right" style="font-size:15px;"></i> </p> </div>
+			<div class="statement">
+	                	<div class="down">
+					<p> We are an <span style="font-weight: 900;">international organization</span> based in <span style="font-weight: 900;">Seoul, South Korea</span>. </p>
+				</div>
+                		<div class="up">
+					<div class="yellow-button"> <p> find us <i class="glyphicon glyphicon-chevron-right" style="font-size:15px;"></i> </p> </div>
+				</div>
 			</div>
 	</div>
 </section>
