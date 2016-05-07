7590f3113059030757c1c69247096d2dd2924ad4
munair <munair@qe.local>
2016-05-07 17:43:07 +0900

    forgot the centering... argh... details

--- templates/banner.php
+++ templates/banner.php
@@ -18,6 +18,7 @@
 		</div>
 	</div>
 	<div class="stack-2">
+		<div class="centering">
 			<div class="statement">
                 		<div class="left">
 					<p> We <span style="font-weight: 900;">collaborate globally</span> to fight <span style="font-weight: 900;">infectious disease</span>. </p>
@@ -26,8 +27,10 @@
 					<div class="yellow-button"> <p> help us <i class="glyphicon glyphicon-chevron-right" style="font-size:15px;"></i> </p> </div>
 				</div>
 			</div>
+		</div>
 	</div>
 	<div class="stack-3">
+		<div class="centering">
 			<div class="statement">
 	                	<div class="down">
 					<p> We are an <span style="font-weight: 900;">international organization</span> based in <span style="font-weight: 900;">Seoul, South Korea</span>. </p>
@@ -36,5 +39,6 @@
 					<div class="yellow-button"> <p> find us <i class="glyphicon glyphicon-chevron-right" style="font-size:15px;"></i> </p> </div>
 				</div>
 			</div>
+		</div>
 	</div>
 </section>
