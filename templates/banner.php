f73b39734ef7037daee522a54c96b3049e3c6580
munair <munair@qe.local>
2016-05-07 15:49:42 +0900

    rolling back... that resulting in the exact situation we are trying to avoid

--- templates/banner.php
+++ templates/banner.php
@@ -5,9 +5,8 @@
 		<li></li>
 		<li></li>
 	</ul>
-	<div class="stack">
-		<div layer-3">
-		<div class="centering">
+	<div class="stack layer-3">
+		<div class="stack centering">
                 	<div class="slide push">
 				<p> We develop <span style="font-weight: 900;">vaccines</span> for <span style="font-weight: 900;">global health</span>. </p>
 			</div>
@@ -15,9 +14,9 @@
 				<div class="yellow-button"> <p> join us <i class="glyphicon glyphicon-chevron-right" style="font-size:15px;"></i> </p> </div>
 			</div>
 		</div>
-		</div>
-		<div layer-2">
-		<div class="centering">
+	</div>
+	<div class="stack layer-2">
+		<div class="stack centering">
                 	<div class="slide left">
 				<p> We <span style="font-weight: 900;">collaborate globally</span> to fight <span style="font-weight: 900;">infectious disease</span>. </p>
 			</div>
@@ -25,16 +24,15 @@
 				<div class="yellow-button"> <p> help us <i class="glyphicon glyphicon-chevron-right" style="font-size:15px;"></i> </p> </div>
 			</div>
 		</div>
-		</div>
-		<div layer-1">
-		<div class="centering">
+	</div>
+	<div class="stack layer-1">
+		<div class="stack centering">
                 	<div class="slide down">
 				<p> We are an <span style="font-weight: 900;">international organization</span> based in <span style="font-weight: 900;">Seoul, South Korea</span>. </p>
 			</div>
                 	<div class="slide up">
 				<div class="yellow-button"> <p> find us <i class="glyphicon glyphicon-chevron-right" style="font-size:15px;"></i> </p> </div>
 			</div>
-		</div>
 		</div>
 	</div>
 </section>
