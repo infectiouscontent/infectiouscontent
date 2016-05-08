f23d47754f3e992033b4e7ffad5afadb70060203
munair <munair@qe.local>
2016-05-08 20:52:59 +0900

    update ribbon

--- templates/ribbon.php
+++ templates/ribbon.php
@@ -4,22 +4,22 @@
 		<div class="row">
 			<div class="col-md-3">
 				<div class="ribbon-text">
-					<h2>How can I use handshaker?</h2>
+					<h2>How can I help?</h2>
 				</div>
 			</div>
 			<div class="col-md-3">
 				<div class="ribbon-text">
-					<p><a href="#"> <img src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/ribbon-howto-1-80x80px.png">Activate the theme </a></p>
+					<p><a href="#"> <span>01/</span> Contribute financially </a></p>
 				</div>
 			</div>
 			<div class="col-md-3">
 				<div class="ribbon-text">
-					<p><a href="#"> <img src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/ribbon-howto-2-80x80px.png">Use the customizer </a></p>
+					<p><a href="#"> <span>02/</span> Collaborate professionally </a></p>
 				</div>
 			</div>
 			<div class="col-md-3">
 				<div class="ribbon-text">
-					<p><a href="#"> <img src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/ribbon-howto-3-80x80px.png">Start posting content </a></p>
+					<p><a href="#"> <span>03/</span> Cover journalistically </a></p>
 				</div>
 			</div>
 		</div>
