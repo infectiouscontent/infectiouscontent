0e50299e2043ccf43ea4b17acab92ca1798335d2
munair <munair@qe.local>
2016-05-09 15:06:30 +0900

    implement correct bootstrap grid

--- templates/about.php
+++ templates/about.php
@@ -7,28 +7,28 @@
 			<p>Discover, develop, and deliver safe, effective and affordable vaccines for global public health.</p>
 		</div>
 		<div class="row">
-			<div class="col-md-4">
+			<div class="col-md-3">
 				<div class="logoContainer">
 					<div class="discoverReveal"> 
 						<div class="discover">Discover</div>
 					</div>
 				</div>
 			</div>
-			<div class="col-md-4">
+			<div class="col-md-3">
 				<div class="logoContainer">
 					<div class="developReveal"> 
 						<div class="develop">Develop</div>
 					</div>
 				</div>
 			</div>
-			<div class="col-md-4">
+			<div class="col-md-3">
 				<div class="logoContainer">
 					<div class="deliverReveal"> 
 						<div class="deliver">Deliver</div>
 					</div>
 				</div>
 			</div>
-			<div class="col-md-4">
+			<div class="col-md-3">
 				<div class="logoContainer">
 					<div class="sustainReveal"> 
 						<div class="dustain">Sustain</div>
