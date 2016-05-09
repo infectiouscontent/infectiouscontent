4709911c2081a4199a75c62d89a722ec2319febb
munair <munair@qe.local>
2016-05-10 06:10:41 +0900

    update formatting

--- templates/introduction.php
+++ templates/introduction.php
@@ -2,21 +2,21 @@
 	<div class="container-fluid">
 		<div class="row">
 			<div class="col-md-4 featured">
-				<div><h1>News</h1></div>
+				<h1>News</h1>
 				<div style="border: 1px #ff6600 solid; background-color: #ffffff; color: #000000;">
 					<h3>Press Releases</h3>
 					<h3>Media Coverage</h3>
 				</div>
 			</div>
 			<div class="col-md-4 featured">
-				<div><h1>On the blog</h1></div>
+				<h1>On the blog</h1>
 				<div style="border: 1px #ff6600 solid; background-color: #ffffff; color: #000000;">
 					<h3>Vaccinology</h3>
 					<h3>Symposia</h3>
 				</div>
 			</div>
 			<div class="col-md-4 featured">
-				<div><h1>Announcements</h1></div>
+				<h1>Announcements</h1>
 				<div style="border: 1px #ff6600 solid; background-color: #ffffff; color: #000000;">
 					<h3>Institutional</h3>
 					<h3>Bids</h3>
