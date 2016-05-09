f657c9f4380a4240275142f33398b3176fa3c3da
munair <munair@qe.local>
2016-05-09 16:08:31 +0900

    fix col-md-offset for ribbon

--- templates/ribbon.php
+++ templates/ribbon.php
@@ -9,19 +9,19 @@
 			</div>
 			<div class="col-md-3">
 				<div class="row ribbon-text">
-					<div class="col-xs-3 col-xs-offset-3 col-md-4 ribbon-number">01/</div>
+					<div class="col-xs-3 col-xs-offset-3 col-md-4 col-md-offset-0 ribbon-number">01/</div>
 					<div class="col-xs-6 col-md-8"><p><a href="#"> Contribute financially </a></p> </div>
 				</div>
 			</div>
 			<div class="col-md-3">
 				<div class="row ribbon-text">
-					<div class="col-xs-3 col-xs-offset-3 col-md-4 ribbon-number">02/</div>
+					<div class="col-xs-3 col-xs-offset-3 col-md-4 col-md-offset-0 ribbon-number">02/</div>
 					<div class="col-xs-6 col-md-8"><p><a href="#"> Collaborate professionally </a></p> </div>
 				</div>
 			</div>
 			<div class="col-md-3">
 				<div class="row ribbon-text">
-					<div class="col-xs-3 col-xs-offset-3 col-md-4 ribbon-number">03/</div>
+					<div class="col-xs-3 col-xs-offset-3 col-md-4 col-md-offset-0 ribbon-number">03/</div>
 					<div class="col-xs-6 col-md-8"><p><a href="#"> Cover journalistically </a></p>
 				</div>
 			</div>
