ccf4dd4664a77431698b2989238c2f8e469845fb
munair <munair@qe.local>
2016-05-09 15:13:52 +0900

    add program element styling

--- templates/about.php
+++ templates/about.php
@@ -10,28 +10,28 @@
 			<div class="col-md-3">
 				<div class="logoContainer">
 					<div class="discoverReveal"> 
-						<div class="discover">Discover</div>
+						<div class="discover program">Discover</div>
 					</div>
 				</div>
 			</div>
 			<div class="col-md-3">
 				<div class="logoContainer">
 					<div class="developReveal"> 
-						<div class="develop">Develop</div>
+						<div class="develop program">Develop</div>
 					</div>
 				</div>
 			</div>
 			<div class="col-md-3">
 				<div class="logoContainer">
 					<div class="deliverReveal"> 
-						<div class="deliver">Deliver</div>
+						<div class="deliver program">Deliver</div>
 					</div>
 				</div>
 			</div>
 			<div class="col-md-3">
 				<div class="logoContainer">
 					<div class="sustainReveal"> 
-						<div class="dustain">Sustain</div>
+						<div class="sustain program">Sustain</div>
 					</div>
 				</div>
 			</div>
