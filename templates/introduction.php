bb9fe7164d054e8eb2b071ea8a9112e398ab9ef8
munair <munair@qe.local>
2016-05-10 05:51:43 +0900

    fix styling of featured items

--- templates/introduction.php
+++ templates/introduction.php
@@ -1,22 +1,22 @@
 <section id="introduction" class="section-introduction">
 	<div class="container-fluid">
 		<div class="row">
-			<div class="col-md-3 col-md-offset-1" style="background-color: #ff6600; padding: 0 0 50px 0;">
-				<div style="border-bottom: 1px white solid;"><h3>News</h3></div>
+			<div class="col-md-3 col-md-offset-1 featured">
+				<div><h3>News</h3></div>
 				<div style="border: 1px #ff6600 solid; background-color: #ffffff; color: #000000;">
 					<h5>Press Releases</h5>
 					<h5>Media Coverage</h5>
 				</div>
 			</div>
-			<div class="col-md-3 col-md-offset-1" style="background-color: #ff6600; padding: 0 0 50px 0;">
-				<div style="border-bottom: 1px white solid;"><h3>On the blog</h3></div>
+			<div class="col-md-3 col-md-offset-1 featured">
+				<div><h3>On the blog</h3></div>
 				<div style="border: 1px #ff6600 solid; background-color: #ffffff; color: #000000;">
 					<h5>Vaccinology</h5>
 					<h5>Symposia</h5>
 				</div>
 			</div>
-			<div class="col-md-3 col-md-offset-1" style="background-color: #ff6600; padding: 0 0 50px 0;">
-				<div style="border-bottom: 1px white solid;"><h3>Announcements</h3></div>
+			<div class="col-md-3 col-md-offset-1 featured">
+				<div><h3>Announcements</h3></div>
 				<div style="border: 1px #ff6600 solid; background-color: #ffffff; color: #000000;">
 					<h5>Institutional</h5>
 					<h5>Bids</h5>
