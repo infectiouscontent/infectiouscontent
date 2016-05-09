65b05d28ced00ab23ecd2089c731baa93b0bd805
munair <munair@qe.local>
2016-05-09 12:08:48 +0900

    add about section

---
+++ templates/about.php
@@ -0,0 +1,23 @@
+<section id="about" class="section-about">
+	<!-- Organization's Executive Summary -->
+	<div class="container-fluid organization-summary">
+		<!-- Mission Statement -->
+		<div class="row">
+			<h1>Our Mission</h1>
+			<p>Discover, develop, and deliver safe, effective and affordable vaccines for global public health.</p>
+		</div>
+		<div class="row">
+			<div class="col-md-4">
+				<div class="logoContainer">
+					<div class="logoReveal"> 
+						<img class="introduction-image" src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/cochin-q-circle-orangeicon-center-1000x1000px.png">
+					</div>
+				</div>
+			</div>
+			<div class="col-xs-12 col-md-6 introduction-title"><div class="titleReveal"><h1>handshaker</h1></div></div>
+			<div class="col-xs-12 col-md-6 introduction-line"><div class="colored-line"></div></div>
+			<div class="col-xs-12 col-md-6 introduction-text"><div class="aboutReveal">Handshaker is a Wordpress Theme created by Quilombola Engineering. 
+			</div>
+		</div>
+	</div>
+</section>
