440aa58b330b382628310d52748596982908783f
munair <munair@qe.local>
2016-05-10 06:00:03 +0900

    fix bootstrab grid

--- templates/introduction.php
+++ templates/introduction.php
@@ -1,21 +1,21 @@
 <section id="introduction" class="section-introduction">
 	<div class="container-fluid">
 		<div class="row">
-			<div class="col-md-3 col-md-offset-1 featured">
+			<div class="col-md-4 featured">
 				<div><h3>News</h3></div>
 				<div style="border: 1px #ff6600 solid; background-color: #ffffff; color: #000000;">
 					<h5>Press Releases</h5>
 					<h5>Media Coverage</h5>
 				</div>
 			</div>
-			<div class="col-md-3 col-md-offset-1 featured">
+			<div class="col-md-4 featured">
 				<div><h3>On the blog</h3></div>
 				<div style="border: 1px #ff6600 solid; background-color: #ffffff; color: #000000;">
 					<h5>Vaccinology</h5>
 					<h5>Symposia</h5>
 				</div>
 			</div>
-			<div class="col-md-3 col-md-offset-1 featured">
+			<div class="col-md-4 featured">
 				<div><h3>Announcements</h3></div>
 				<div style="border: 1px #ff6600 solid; background-color: #ffffff; color: #000000;">
 					<h5>Institutional</h5>
