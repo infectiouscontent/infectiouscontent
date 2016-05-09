6982ea1a4ad104e23c04244bbaca1a2fe2497309
munair <munair@qe.local>
2016-05-10 05:44:26 +0900

    add introduction

--- templates/introduction.php
+++ templates/introduction.php
@@ -1,16 +1,26 @@
 <section id="introduction" class="section-introduction">
 	<div class="container-fluid">
 		<div class="row">
-			<div class="col-xs-12">
-				<h1>Introduction</h1>
-				<p>This is a compelling story about Handshaker and its mission to change the world. In this section I will make you fall in love with Handshaker. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. <i>Lorem ipsum dolor sit amet, consectetur adipiscing elit</i>. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. <i>Lorem ipsum dolor sit amet, consectetur adipiscing elit</i>. Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. </p>
-				
-				<p>Quisque volutpat condimentum velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. <b>Maecenas mattis</b>. Nam nec ante. Sed lacinia, urna non tincidunt mattis, tortor neque adipiscing diam, a cursus ipsum ante quis turpis. Nulla facilisi. Ut fringilla. Suspendisse potenti. <i>Aenean quam</i>. Nunc feugiat mi a tellus consequat imperdiet. Vestibulum sapien. Proin quam. Etiam ultrices. </p>
-				
-				<p><b>Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh</b>. Suspendisse in justo eu magna luctus suscipit. Sed lectus. Integer euismod lacus luctus magna. <b>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos</b>. Quisque cursus, metus vitae pharetra auctor, sem massa mattis sem, at interdum magna augue eget diam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi lacinia molestie dui. Praesent blandit dolor. Sed non quam. In vel mi sit amet augue congue elementum. Morbi in ipsum sit amet pede facilisis laoreet. Donec lacus nunc, viverra nec, blandit vel, egestas et, augue. Vestibulum tincidunt malesuada tellus. Ut ultrices ultrices enim. </p>
-				
-				<p>Curabitur sit amet mauris. Morbi in dui quis est pulvinar ullamcorper. <i>Quisque volutpat condimentum velit</i>. Nulla facilisi. Integer lacinia sollicitudin massa. Cras metus. Sed aliquet risus a tortor. Integer id quam. Morbi mi. <i>Sed non quam</i>. Quisque nisl felis, venenatis tristique, dignissim in, ultrices sit amet, augue. Proin sodales libero eget ante. Nulla quam. Aenean laoreet. Vestibulum nisi lectus, commodo ac, facilisis ac, ultricies eu, pede. </p>
-			<div class="yellow-button">Read More</div>
+			<div class="col-md-3 col-md-offset-1" style="background-color: #ff6600; padding: 0 0 50px 0;">
+				<div style="border-bottom: 1px white solid;"><h3>News</h3></div>
+				<div style="border: 1px #ff6600 solid; background-color: #ffffff; color: #000000;">
+					<h5>Press Releases</h5>
+					<h5>Media Coverage</h5>
+				</div>
+			</div>
+			<div class="col-md-3 col-md-offset-1" style="background-color: #ff6600; padding: 0 0 50px 0;">
+				<div style="border-bottom: 1px white solid;"><h3>On the blog</h3></div>
+				<div style="border: 1px #ff6600 solid; background-color: #ffffff; color: #000000;">
+					<h5>Vaccinology</h5>
+					<h5>Symposia</h5>
+				</div>
+			</div>
+			<div class="col-md-3 col-md-offset-1" style="background-color: #ff6600; padding: 0 0 50px 0;">
+				<div style="border-bottom: 1px white solid;"><h3>Announcements</h3></div>
+				<div style="border: 1px #ff6600 solid; background-color: #ffffff; color: #000000;">
+					<h5>Institutional</h5>
+					<h5>Bids</h5>
+				</div>
 			</div>
 		</div>
 	</div>
