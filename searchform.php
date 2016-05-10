6c44a1a96490c708c0121f42bd45e854ebd45443
munair <munair@qe.local>
2016-05-10 18:13:48 +0900

    test <span class="glyphicon glyphicon-search">

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
 <form id="search" name="catsearchform47525" method="post" action="&nbsp;" class="search-form">
     <div class="search-box">
         <input class="w-input search-input" onchange="updateForm()" type="text" name="CAT_Search" id="searchterm">
-        <input name="submit" class="cat_btn_search" src="http://ghtcoalition.businesscatalyst.com/images/search-icn.png" width="40" height="40" type="image">    
+        <input name="submit" class="cat_btn_search glyphicon glyphicon-search">    
     </div>
 </form>
