42ae472ec6628bd90e23a681feaecd3f690aa007
munair <munair@qe.local>
2016-05-10 11:36:39 +0900

    start working on sponsors

--- templates/sponsors.php
+++ templates/sponsors.php
@@ -3,14 +3,20 @@
 		<h1>Sponsors</h1>
 		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
 		<div class="row">
-			<div class="col-xs-4 col-md-2 col-md-offset-3"> 
-				<img src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/zumbcdo-square-icon-550x550px.png">
+			<div class="col-md-2 col-md-offset-1"> 
+				<img src="https://s3.ap-northeast-2.amazonaws.com/international-vaccine-institute/swede-smalllogo-495x495px.png">
 			</div>
-			<div class="col-xs-4 col-md-2"> 	
-				<img src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/zhang-simplified-aplha-logo.png">
+			<div class="col-md-2"> 	
+				<img src="https://s3.ap-northeast-2.amazonaws.com/international-vaccine-institute/gatesfoundation-white-2100x2100.png">
 			</div>
-			<div class="col-xs-4 col-md-2"> 
-				<img src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/capoeirastudio-icon-644x644px.png">
+			<div class="col-md-2"> 
+				<img src="https://s3.ap-northeast-2.amazonaws.com/international-vaccine-institute/korean-flag-alpha.png">
+			</div>
+			<div class="col-md-2"> 
+				<img src="https://s3.ap-northeast-2.amazonaws.com/international-vaccine-institute/ksc-ivi.png">
+			</div>
+			<div class="col-md-2"> 
+				<img src="https://s3.ap-northeast-2.amazonaws.com/international-vaccine-institute/continents-1300x1300px.png">
 			</div>
 		</div>
 	</div>
