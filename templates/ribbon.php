764b63569e6abaa3662d506e649958acef2be3e4
munair <munair@qe.local>
2016-05-10 17:49:17 +0900

    update ribbon

--- templates/ribbon.php
+++ templates/ribbon.php
@@ -4,25 +4,25 @@
 		<div class="row">
 			<div class="col-md-3">
 				<div class="ribbon-text">
-					<h2>How can I help?</h2>
+					<h2>How can I get involved?</h2>
 				</div>
 			</div>
 			<div class="col-md-3">
 				<div class="row ribbon-text">
 					<div class="col-xs-3 col-xs-offset-3 col-md-4 col-md-offset-0 ribbon-number">01/</div>
-					<div class="col-xs-6 col-md-8"><p><a href="#"> Contribute financially </a></p> </div>
+					<div class="col-xs-6 col-md-8"><p><a href="#"> Contribute </a></p> </div>
 				</div>
 			</div>
 			<div class="col-md-3">
 				<div class="row ribbon-text">
 					<div class="col-xs-3 col-xs-offset-3 col-md-4 col-md-offset-0 ribbon-number">02/</div>
-					<div class="col-xs-6 col-md-8"><p><a href="#"> Collaborate professionally </a></p> </div>
+					<div class="col-xs-6 col-md-8"><p><a href="#"> Collaborate </a></p> </div>
 				</div>
 			</div>
 			<div class="col-md-3">
 				<div class="row ribbon-text">
 					<div class="col-xs-3 col-xs-offset-3 col-md-4 col-md-offset-0 ribbon-number">03/</div>
-					<div class="col-xs-6 col-md-8"><p><a href="#"> Cover journalistically </a></p>
+					<div class="col-xs-6 col-md-8"><p><a href="#"> Cover </a></p>
 				</div>
 			</div>
 		</div>
