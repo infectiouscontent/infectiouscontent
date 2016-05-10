51411ffe2845efa94198acc9f94c54f779995abe
munair <munair@qe.local>
2016-05-10 11:52:01 +0900

    reload with updated copy

--- templates/sponsors.php
+++ templates/sponsors.php
@@ -1,7 +1,7 @@
 <section id="section-sponsors">
 	<div class="container-fluid sponsors">
-		<h1>Sponsors</h1>
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+		<h1>Our Friends and Supporters</h1>
+		<p>“No act of kindness, no matter how small, is ever wasted.” – Aesop</p>
 		<div class="row">
 			<div class="col-md-2 col-md-offset-1"> 
 				<img src="https://s3.ap-northeast-2.amazonaws.com/international-vaccine-institute/swede-smalllogo-495x495px.png">
