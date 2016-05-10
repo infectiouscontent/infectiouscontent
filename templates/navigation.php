073da833089ed3dfa37fcc8714bcb487f111335c
munair <munair@qe.local>
2016-05-10 18:52:39 +0900

    going

--- templates/navigation.php
+++ templates/navigation.php
@@ -1,12 +1,9 @@
 <section id="navigation" class="section-navigation">
     <div class="row">
+	<div class="col-xs-4">
         <a class="logo" href="#" style="text-decoration: none;"><img src="https://s3.ap-northeast-2.amazonaws.com/international-vaccine-institute/ivi-whitelogo-2400x600px.png"></a>
-
-        <div class="mobile-toggle">
-            <span></span>
-            <span></span>
-            <span></span>
-        </div>
+	</div>
+	<div class="col-xs-4">
         <nav>
             <ul>
                 <li><a href="#" style="text-decoration: none;"><span class="glyphicon glyphicon-bullhorn" aria-hidden="true"></span> Jobs</a></li>
@@ -18,5 +15,13 @@
                 <li><?php get_search_form(); ?></li>
             </ul>
         </nav>
+	</div>
+	<div class="col-xs-4">
+        <div class="mobile-toggle">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+	</div>
     </div> <!-- end row -->
 </section>
