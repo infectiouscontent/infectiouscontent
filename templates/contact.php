a610abaf54572416c5c95e35a9d9cbe9aa4da905
munair <munair@qe.local>
2016-05-10 13:14:24 +0900

    demo time

--- templates/contact.php
+++ templates/contact.php
@@ -12,13 +12,11 @@
 					<h1>Contact Us</h1>
 					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
 				</div>
-                		<div class="col-md-4 col-md-offset-4">
-					<a href="#ribbon"><img class="scroll-down" src="https://s3.ap-northeast-2.amazonaws.com/international-vaccine-institute/ivi-icon-1450x1450px.png" /></a>
+					<a href="#ribbon"><img class="scroll-down" src="https://s3.ap-northeast-2.amazonaws.com/international-vaccine-institute/korean-flag-2000x2000px.png" /></a>
 					<h1>IVI</h1>
 					<p>International Vaccine Institute</p>
 					<p class="contributor-intro">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
 					<div class="yellow-button">More Info</div>
-				</div>
 			</div>
 		</div>
         </div>
