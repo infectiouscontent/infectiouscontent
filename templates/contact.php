72997c01b67b414826bae450885c7f338552fc6c
munair <munair@qe.local>
2016-05-10 12:59:52 +0900

    update contact section

--- templates/contact.php
+++ templates/contact.php
@@ -9,27 +9,13 @@
 		<div class="container-fluid">
 			<div class="row">
 				<div class="title">
-					<h1>Principal Artists</h1>
+					<h1>Contact Us</h1>
 					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
 				</div>
-                		<div class="col-md-4">
-					<a href="#ribbon"><img class="scroll-down" src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/headshot-zumbi-441x441px.png" /></a>
-					<h1>Zumbi</h1>
-					<p>Marketing & Content Development</p>
-					<p class="contributor-intro">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-					<div class="yellow-button">More Info</div>
-				</div>
-                		<div class="col-md-4">
-					<a href="#ribbon"><img class="scroll-down" src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/zhangkaiqin-train-600x600px.png" /></a>
-					<h1>Kai Qin</h1>
-					<p>Financing & Art Curation</p>
-					<p class="contributor-intro">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-					<div class="yellow-button">More Info</div>
-				</div>
-				<div class="col-md-4">
-					<a href="#ribbon"><img class="scroll-down" src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/soyoon-1000x1000px.png" /></a>
-					<h1>Soyoon</h1>
-					<p>Portfolio & Graphic Management</p>
+                		<div class="col-md-4 col-md-offset-4">
+					<a href="#ribbon"><img class="scroll-down" src="https://s3.ap-northeast-2.amazonaws.com/international-vaccine-institute/ivi-icon-1450x1450px.png" /></a>
+					<h1>IVI</h1>
+					<p>International Vaccine Institute</p>
 					<p class="contributor-intro">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
 					<div class="yellow-button">More Info</div>
 				</div>
