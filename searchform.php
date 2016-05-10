1189628f3a9c6b4b0c88700747b9ef504c485500
munair <munair@qe.local>
2016-05-10 18:26:02 +0900

    last try

--- searchform.php
+++ searchform.php
@@ -1,6 +1,5 @@
 <form id="search" name="catsearchform47525" method="post" action="&nbsp;" class="search-form">
     <div class="search-box">
-        <input class="w-input search-input" onchange="updateForm()" type="text" name="CAT_Search" id="searchterm">
-        <input name="submit" class="cat_btn_search"><span class="glyphicon glyphicon-search"></input>
+        <input class="w-input search-input" onchange="updateForm()" type="text" name="CAT_Search" id="searchterm"> <span class="glyphicon glyphicon-search"> </input>
     </div>
 </form>
