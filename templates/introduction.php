bf8dc1e02e100b5a09ff2837a40eabb25ba3d792
munair <munair@qe.local>
2016-05-10 10:59:25 +0900

    working on specificity bug

--- templates/introduction.php
+++ templates/introduction.php
@@ -3,7 +3,7 @@
 		<div class="row">
 			<div class="col-md-4">
 				<div class="featured">
-				<h2>News</h2>
+					<h2>News</h2>
 					<div class="sections">
 						<h4>Press Releases</h4>
 						<h4>Media Coverage</h4>
@@ -12,7 +12,7 @@
 			</div>
 			<div class="col-md-4">
 				<div class="featured">
-				<h2>On the blog</h2>
+					<h2>On the blog</h2>
 					<div class="sections">
 						<h4>Vaccinology</h4>
 						<h4>Symposia</h4>
@@ -21,7 +21,7 @@
 			</div>
 			<div class="col-md-4">
 				<div class="featured">
-				<h2>Announcements</h2>
+					<h2>Announcements</h2>
 					<div class="sections">
 						<h4>Institutional</h4>
 						<h4>Bids</h4>
