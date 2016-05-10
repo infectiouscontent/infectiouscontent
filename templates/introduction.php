6729c4fe18c56901ed4735dfdd89821c1f27263b
munair <munair@qe.local>
2016-05-10 10:02:21 +0900

    insert div

--- templates/introduction.php
+++ templates/introduction.php
@@ -1,25 +1,31 @@
 <section id="introduction" class="section-introduction">
 	<div class="container-fluid">
 		<div class="row">
-			<div class="col-md-4 featured">
+			<div class="col-md-4">
+				<div class="featured">
 				<h1>News</h1>
 				<div style="border: 1px #ff6600 solid; background-color: #ffffff; color: #000000;">
 					<h3>Press Releases</h3>
 					<h3>Media Coverage</h3>
 				</div>
+				</div>
 			</div>
-			<div class="col-md-4 featured">
+			<div class="col-md-4">
+				<div class="featured">
 				<h1>On the blog</h1>
 				<div style="border: 1px #ff6600 solid; background-color: #ffffff; color: #000000;">
 					<h3>Vaccinology</h3>
 					<h3>Symposia</h3>
 				</div>
+				</div>
 			</div>
-			<div class="col-md-4 featured">
+			<div class="col-md-4">
+				<div class="featured">
 				<h1>Announcements</h1>
 				<div style="border: 1px #ff6600 solid; background-color: #ffffff; color: #000000;">
 					<h3>Institutional</h3>
 					<h3>Bids</h3>
+				</div>
 				</div>
 			</div>
 		</div>
