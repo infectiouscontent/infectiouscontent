71e76dcc4df5b961993963e7e9bd2651860c4a53
munair <munair@qe.local>
2016-05-10 12:34:05 +0900

    testing new layout

---
+++ templates/artists.php
@@ -0,0 +1,39 @@
+<section id="contact" class="section-contact">
+        <!-- slider animation -->
+        <ul class="horizontal-slider">
+                <li></li>
+                <li></li>
+                <li></li>
+        </ul>
+        <div class="contributor-overlay">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="title">
+					<h1>Principal Artists</h1>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+				</div>
+                		<div class="col-md-4">
+					<a href="#ribbon"><img class="scroll-down" src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/headshot-zumbi-441x441px.png" /></a>
+					<h1>Zumbi</h1>
+					<p>Marketing & Content Development</p>
+					<p class="contributor-intro">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+					<div class="yellow-button">More Info</div>
+				</div>
+                		<div class="col-md-4">
+					<a href="#ribbon"><img class="scroll-down" src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/zhangkaiqin-train-600x600px.png" /></a>
+					<h1>Kai Qin</h1>
+					<p>Financing & Art Curation</p>
+					<p class="contributor-intro">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+					<div class="yellow-button">More Info</div>
+				</div>
+				<div class="col-md-4">
+					<a href="#ribbon"><img class="scroll-down" src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/soyoon-1000x1000px.png" /></a>
+					<h1>Soyoon</h1>
+					<p>Portfolio & Graphic Management</p>
+					<p class="contributor-intro">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+					<div class="yellow-button">More Info</div>
+				</div>
+			</div>
+		</div>
+        </div>
+</section>
