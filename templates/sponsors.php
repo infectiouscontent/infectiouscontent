3387bfadfa2fd23fc115eefc67ce0689449e1c05
munair <munair@qe.local>
2016-05-10 11:55:51 +0900

    put logos on one line on mobile dispalys

--- templates/sponsors.php
+++ templates/sponsors.php
@@ -3,19 +3,19 @@
 		<h1>Our Friends and Supporters</h1>
 		<p>“No act of kindness, no matter how small, is ever wasted.” – Aesop</p>
 		<div class="row">
-			<div class="col-md-2 col-md-offset-1"> 
+			<div class="col-xs-2 col-xs-offset-1"> 
 				<img src="https://s3.ap-northeast-2.amazonaws.com/international-vaccine-institute/swede-smalllogo-495x495px.png">
 			</div>
-			<div class="col-md-2"> 	
+			<div class="col-xs-2"> 	
 				<img src="https://s3.ap-northeast-2.amazonaws.com/international-vaccine-institute/gatesfoundation-white-2100x2100.png">
 			</div>
-			<div class="col-md-2"> 
+			<div class="col-xs-2"> 
 				<img src="https://s3.ap-northeast-2.amazonaws.com/international-vaccine-institute/korean-flag-2000x2000px.png">
 			</div>
-			<div class="col-md-2"> 
+			<div class="col-xs-2"> 
 				<img src="https://s3.ap-northeast-2.amazonaws.com/international-vaccine-institute/ksc-ivi.png">
 			</div>
-			<div class="col-md-2"> 
+			<div class="col-xs-2"> 
 				<img src="https://s3.ap-northeast-2.amazonaws.com/international-vaccine-institute/continents-1300x1300px.png">
 			</div>
 		</div>
