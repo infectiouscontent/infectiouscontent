3c52a6567afd314d5855502fe84f506ad00ae554
munair <munair@qe.local>
2016-05-10 10:47:37 +0900

    testing specificity

--- templates/introduction.php
+++ templates/introduction.php
@@ -4,28 +4,28 @@
 			<div class="col-md-4">
 				<div class="featured">
 				<h2>News</h2>
-				<div style="border: 1px #ff6600 solid; background-color: #ffffff; color: #000000;">
-					<h4>Press Releases</h4>
-					<h4>Media Coverage</h4>
-				</div>
+					<div class="sections">
+						<h4>Press Releases</h4>
+						<h4>Media Coverage</h4>
+					</div>
 				</div>
 			</div>
 			<div class="col-md-4">
 				<div class="featured">
 				<h2>On the blog</h2>
-				<div style="border: 1px #ff6600 solid; background-color: #ffffff; color: #000000;">
-					<h4>Vaccinology</h4>
-					<h4>Symposia</h4>
-				</div>
+					<div class="sections">
+						<h4>Vaccinology</h4>
+						<h4>Symposia</h4>
+					</div>
 				</div>
 			</div>
 			<div class="col-md-4">
 				<div class="featured">
 				<h2>Announcements</h2>
-				<div style="border: 1px #ff6600 solid; background-color: #ffffff; color: #000000;">
-					<h4>Institutional</h4>
-					<h4>Bids</h4>
-				</div>
+					<div class="sections">
+						<h4>Institutional</h4>
+						<h4>Bids</h4>
+					</div>
 				</div>
 			</div>
 		</div>
