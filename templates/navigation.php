3e03a874b5e19bed2aa1f00a10169b01fa4902c2
munair <munair@qe.local>
2016-05-11 17:50:21 +0900

    left out container fluid silly

--- templates/navigation.php
+++ templates/navigation.php
@@ -1,25 +1,27 @@
 <section id="navigation" class="section-navigation">
-    <div class="row">
-	<div class="col-xs-4 col-sm-3 col-md-2">
-        <a class="logo" href="#" style="text-decoration: none;"><img src="https://s3.ap-northeast-2.amazonaws.com/international-vaccine-institute/ivi-whitelogo-2400x600px.png"></a>
-	</div>
-	<div class="col-xs-8 col-sm-9 col-md-10">
-        <nav>
-            <ul>
-                <li><a href="#" style="text-decoration: none;"> Jobs</a></li>
-                <li><a href="#" style="text-decoration: none;"> News</a></li>
-                <li><a href="#" style="text-decoration: none;"> Contact Us</a></li>
-                <li><a href="#" style="text-decoration: none;"> Support IVI</a></li>
-                <li><a href="#contact" style="text-decoration: none;"> Sitemap</a></li>
-                <li><a href="#" style="text-decoration: none;"> Korean</a></li>
-                <li><?php get_search_form(); ?></li>
-            </ul>
-        </nav>
-        <div class="mobile-toggle">
-            <span></span>
-            <span></span>
-            <span></span>
-        </div>
+    <div class="container-fluid">
+    	<div class="row">
+		<div class="col-xs-4 col-sm-3 col-md-2">
+	        	<a class="logo" href="#" style="text-decoration: none;"><img src="https://s3.ap-northeast-2.amazonaws.com/international-vaccine-institute/ivi-whitelogo-2400x600px.png"></a>
+		</div>
+		<div class="col-xs-8 col-sm-9 col-md-10">
+	        	<nav>
+		            <ul>
+		                <li><a href="#" style="text-decoration: none;"> Jobs</a></li>
+		                <li><a href="#" style="text-decoration: none;"> News</a></li>
+		                <li><a href="#" style="text-decoration: none;"> Contact Us</a></li>
+		                <li><a href="#" style="text-decoration: none;"> Support IVI</a></li>
+		                <li><a href="#contact" style="text-decoration: none;"> Sitemap</a></li>
+		                <li><a href="#" style="text-decoration: none;"> Korean</a></li>
+		                <li><?php get_search_form(); ?></li>
+		            </ul>
+	       	 	</nav>
+		        <div class="mobile-toggle">
+		            <span></span>
+		            <span></span>
+		            <span></span>
+		        </div>
+		</div>
 	</div>
     </div> <!-- end row -->
 </section>
