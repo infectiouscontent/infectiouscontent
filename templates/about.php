9eeca3de95069f2585d2925590a5b821a8b24ff4
munair <munair@qe.local>
2016-05-11 17:55:12 +0900

    some how the changes were not committed... err... it was my mistake.

--- templates/about.php
+++ templates/about.php
@@ -7,28 +7,28 @@
 			<p>Discover, develop, and deliver safe, effective and affordable vaccines for global public health.</p>
 		</div>
 		<div class="row">
-			<div class="col-md-3">
+			<div class="col-sm-3">
 				<div class="logoContainer">
 					<div class="discoverReveal"> 
 						<div class="program"><div class="discover"><p>Discover</p></div></div>
 					</div>
 				</div>
 			</div>
-			<div class="col-md-3">
+			<div class="col-sm-3">
 				<div class="logoContainer">
 					<div class="developReveal"> 
 						<div class="program"><div class="develop"><p>Develop</p></div></div>
 					</div>
 				</div>
 			</div>
-			<div class="col-md-3">
+			<div class="col-sm-3">
 				<div class="logoContainer">
 					<div class="deliverReveal"> 
 						<div class="program"><div class="deliver"><p>Deliver</p></div></div>
 					</div>
 				</div>
 			</div>
-			<div class="col-md-3">
+			<div class="col-sm-3">
 				<div class="logoContainer">
 					<div class="sustainReveal"> 
 						<div class="program"><div class="sustain"><p>Sustain</p></div></div>
