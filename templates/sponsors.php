afc91cf6bc6733d12fc86dd276457c5de74852fc
munair <munair@qe.local>
2016-05-11 18:12:38 +0900

    poorly formatted. doing over.

--- templates/sponsors.php
+++ templates/sponsors.php
@@ -1,19 +1,19 @@
 <section id="section-sponsors">
 	<div class="container-fluid sponsors">
 		<h1>Our Friends and Supporters</h1>
-		<div class="row">
-			<div class="col-xs-5 col-xs-offset-1 col-sm-3"> 
+		<div class="row"><div class="col-xs-10 col-xs-offset-1"><div class="row">
+			<div class="col-xs-3"> 
 				<img src="https://s3.ap-northeast-2.amazonaws.com/international-vaccine-institute/swede-smalllogo-495x495px.png">
 			</div>
-			<div class="col-xs-5 col-sm-3"> 	
+			<div class="col-xs-3"> 	
 				<img src="https://s3.ap-northeast-2.amazonaws.com/international-vaccine-institute/gatesfoundation-white-2100x2100.png">
 			</div>
-			<div class="col-xs-5 col-xs-offset-1 col-sm-3"> 
+			<div class="col-xs-3"> 
 				<img src="https://s3.ap-northeast-2.amazonaws.com/international-vaccine-institute/korean-flag-2000x2000px.png">
 			</div>
-			<div class="col-xs-5 col-sm-3"> 
+			<div class="col-xs-3"> 
 				<img src="https://s3.ap-northeast-2.amazonaws.com/international-vaccine-institute/ksc-ivi.png">
 			</div>
-		</div>
+		</div></div></div>
 	</div>
 </section>
