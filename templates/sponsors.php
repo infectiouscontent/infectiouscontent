95ff0f923d9825cb69b971671f67bfec66be1746
munair <munair@qe.local>
2016-05-11 18:05:08 +0900

    now format the sponsor section for small devices and up

--- templates/sponsors.php
+++ templates/sponsors.php
@@ -2,16 +2,16 @@
 	<div class="container-fluid sponsors">
 		<h1>Our Friends and Supporters</h1>
 		<div class="row">
-			<div class="col-xs-5 col-xs-offset-1"> 
+			<div class="col-xs-5 col-xs-offset-1 col-sm-3"> 
 				<img src="https://s3.ap-northeast-2.amazonaws.com/international-vaccine-institute/swede-smalllogo-495x495px.png">
 			</div>
-			<div class="col-xs-5"> 	
+			<div class="col-xs-5 col-sm-3"> 	
 				<img src="https://s3.ap-northeast-2.amazonaws.com/international-vaccine-institute/gatesfoundation-white-2100x2100.png">
 			</div>
-			<div class="col-xs-5 col-xs-offset-1"> 
+			<div class="col-xs-5 col-xs-offset-1 col-sm-3"> 
 				<img src="https://s3.ap-northeast-2.amazonaws.com/international-vaccine-institute/korean-flag-2000x2000px.png">
 			</div>
-			<div class="col-xs-5"> 
+			<div class="col-xs-5 col-sm-3"> 
 				<img src="https://s3.ap-northeast-2.amazonaws.com/international-vaccine-institute/ksc-ivi.png">
 			</div>
 		</div>
