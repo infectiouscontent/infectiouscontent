39f0f5de9f62cb53a8e1ba644e253eaa28a8e697
munair <munair@qe.local>
2016-05-11 15:09:32 +0900

    remove the ultimate div in the navigation bar because it is eating up precious real estate

--- templates/navigation.php
+++ templates/navigation.php
@@ -1,6 +1,6 @@
 <section id="navigation" class="section-navigation">
     <div class="row">
-	<div class="col-xs-1">
+	<div class="col-xs-2">
         <a class="logo" href="#" style="text-decoration: none;"><img src="https://s3.ap-northeast-2.amazonaws.com/international-vaccine-institute/ivi-whitelogo-2400x600px.png"></a>
 	</div>
 	<div class="col-xs-10">
@@ -15,8 +15,6 @@
                 <li><?php get_search_form(); ?></li>
             </ul>
         </nav>
-	</div>
-	<div class="col-xs-1">
         <div class="mobile-toggle">
             <span></span>
             <span></span>
