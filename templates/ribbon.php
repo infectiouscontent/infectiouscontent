622d148873e348e024551416aceca068da1927a5
munair <munair@qe.local>
2016-05-11 17:40:29 +0900

    target small devices as it formats fine on extra small devices

--- templates/ribbon.php
+++ templates/ribbon.php
@@ -7,19 +7,19 @@
 					<h2>How can I get involved?</h2>
 				</div>
 			</div>
-			<div class="col-xs-4 col-md-3">
+			<div class="col-sm-4 col-md-3">
 				<div class="row ribbon-text">
 					<div class="col-xs-3 col-xs-offset-3 col-md-4 col-md-offset-0 ribbon-number">01/</div>
 					<div class="col-xs-6 col-md-8"><p><a href="#"> Contribute </a></p> </div>
 				</div>
 			</div>
-			<div class="col-xs-4 col-md-3">
+			<div class="col-sm-4 col-md-3">
 				<div class="row ribbon-text">
 					<div class="col-xs-3 col-xs-offset-3 col-md-4 col-md-offset-0 ribbon-number">02/</div>
 					<div class="col-xs-6 col-md-8"><p><a href="#"> Collaborate </a></p> </div>
 				</div>
 			</div>
-			<div class="col-xs-4 col-md-3">
+			<div class="col-sm-4 col-md-3">
 				<div class="row ribbon-text">
 					<div class="col-xs-3 col-xs-offset-3 col-md-4 col-md-offset-0 ribbon-number">03/</div>
 					<div class="col-xs-6 col-md-8"><p><a href="#"> Cover </a></p>
