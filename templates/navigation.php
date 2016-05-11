b690e5c272ed4da1abbf79804c8a2b2f8c4adc36
munair <munair@qe.local>
2016-05-11 16:14:53 +0900

    fix the mobile display by using xs columns that take up the whole row. however, ensure that only 50% of the logo is displayed on mobile devices.

--- templates/navigation.php
+++ templates/navigation.php
@@ -1,18 +1,18 @@
 <section id="navigation" class="section-navigation">
     <div class="row">
-	<div class="col-xs-2">
+	<div class="col-md-2">
         <a class="logo" href="#" style="text-decoration: none;"><img src="https://s3.ap-northeast-2.amazonaws.com/international-vaccine-institute/ivi-whitelogo-2400x600px.png"></a>
 	</div>
-	<div class="col-xs-10">
+	<div class="col-md-10">
         <nav>
             <ul>
-                <li><a href="#" style="text-decoration: none;"><span class="glyphicon glyphicon-bullhorn" aria-hidden="true"></span> Jobs</a></li>
-                <li><a href="#" style="text-decoration: none;"><span class="glyphicon glyphicon-info-sign" aria-hidden="true"></span> News</a></li>
-                <li><a href="#" style="text-decoration: none;"><span class="glyphicon glyphicon-picture" aria-hidden="true"></span> Contact Us</a></li>
-                <li><a href="#" style="text-decoration: none;"><span class="glyphicon glyphicon-bullhorn" aria-hidden="true"></span> Support IVI</a></li>
-                <li><a href="#contact" style="text-decoration: none;"><span class="glyphicon glyphicon-picture" aria-hidden="true"></span> Sitemap</a></li>
-                <li><a href="#" style="text-decoration: none;"><span class="glyphicon glyphicon-picture" aria-hidden="true"></span> Korean</a></li>
-                <li><div class="search-form"><?php get_search_form(); ?></div></li>
+                <li><a href="#" style="text-decoration: none;"> Jobs</a></li>
+                <li><a href="#" style="text-decoration: none;"> News</a></li>
+                <li><a href="#" style="text-decoration: none;"> Contact Us</a></li>
+                <li><a href="#" style="text-decoration: none;"> Support IVI</a></li>
+                <li><a href="#contact" style="text-decoration: none;"> Sitemap</a></li>
+                <li><a href="#" style="text-decoration: none;"> Korean</a></li>
+                <li><?php get_search_form(); ?></li>
             </ul>
         </nav>
         <div class="mobile-toggle">
