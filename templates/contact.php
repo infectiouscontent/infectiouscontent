d78dfb34345789d4cd6f935c1ce605c1425c55b9
munair <munair@qe.local>
2016-05-20 09:49:05 +0900

    change h2 color

--- templates/contact.php
+++ templates/contact.php
@@ -3,7 +3,7 @@
 			<div class="row">
 				<div class="title">
 					<h1>Contact Us</h1>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+					<p>The Internation Vaccine Institute is based in Seoul, Korea. To visit our offices please refer to the information below:</p>
 				</div>
 				<a href="#ribbon"><img class="scroll-down" src="https://s3.ap-northeast-2.amazonaws.com/international-vaccine-institute/ivi-icon-1450x1450px.png" /></a>
 				<h1>IVI</h1>
