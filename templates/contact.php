684256afedbd4fb761fc9e3a3f17a7869e455cc7
munair <munair@qe.local>
2016-05-20 10:35:02 +0900

    remove old connect section

--- templates/contact.php
+++ templates/contact.php
@@ -9,16 +9,6 @@
 				<h1>IVI</h1>
 				<p>International Vaccine Institute</p>
 				<p class="address">SNU Research Park, 1 Gwanak-ro, Gwanak-gu, Seoul, 151-742 Korea TEL:02-872-2801, FAX:02-872-2803 </p>
-				<div class="connect">
-		                	<h2>Connect</h2>
-			                <p class="about-text">Connect with the Internation Vaccine Institue via social media:</p>
-					<div onmousedown="sploob()" class="col-xs-2"><span class="glyphicon glyphicon-link"></span></div>
-					<div onmousedown="sploob()" class="col-xs-2"><span class="glyphicon glyphicon-search"></span></div>
-					<div onmousedown="sploob()" class="col-xs-2"><span class="glyphicon glyphicon-user"></span></div>
-					<div onmousedown="sploob()" class="col-xs-2"><span class="glyphicon glyphicon-phone"></span></div>
-					<div onmousedown="sploob()" class="col-xs-2"><span class="glyphicon glyphicon-fire"></span></div>
-					<div onmousedown="sploob()" class="col-xs-2"><span class="glyphicon glyphicon-envelope"></span></div>
-				</div>
 			</div>
 		</div>
 </section>
