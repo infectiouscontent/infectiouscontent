287c9b33d4ba00a4c95ef0a6a3bd09784d9b6c4f
munair <munair@qe.local>
2016-05-20 10:08:33 +0900

    implement paypal button

--- templates/contact.php
+++ templates/contact.php
@@ -8,10 +8,10 @@
 				<a href="#ribbon"><img class="scroll-down" src="https://s3.ap-northeast-2.amazonaws.com/international-vaccine-institute/ivi-icon-1450x1450px.png" /></a>
 				<h1>IVI</h1>
 				<p>International Vaccine Institute</p>
-				<p class="address">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+				<p class="address">SNU Research Park, 1 Gwanak-ro, Gwanak-gu, Seoul, 151-742 Korea TEL:02-872-2801, FAX:02-872-2803 </p>
 				<div class="connect">
 		                	<h2>Connect</h2>
-			                <p class="about-text">Learn more about Handshaker and Quilombola Engineering:</p>
+			                <p class="about-text">Connect with the Internation Vaccine Institue via social media:</p>
 					<div onmousedown="sploob()" class="col-xs-2"><span class="glyphicon glyphicon-link"></span></div>
 					<div onmousedown="sploob()" class="col-xs-2"><span class="glyphicon glyphicon-search"></span></div>
 					<div onmousedown="sploob()" class="col-xs-2"><span class="glyphicon glyphicon-user"></span></div>
