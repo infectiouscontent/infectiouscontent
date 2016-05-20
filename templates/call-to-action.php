e7188ec9472857d356b7091c60da612fc3cac21d
munair <munair@qe.local>
2016-05-20 10:27:18 +0900

    redo

--- templates/call-to-action.php
+++ templates/call-to-action.php
@@ -24,7 +24,16 @@
 				</div>
 			</div>
 			<div class="row">
-
+                                <div class="connect">
+                                        <h2>Connect</h2>
+                                        <p class="about-text">Connect with the Internation Vaccine Institue via social media:</p>
+                                        <div onmousedown="sploob()" class="col-xs-2"><span class="glyphicon glyphicon-link"></span></div>
+                                        <div onmousedown="sploob()" class="col-xs-2"><span class="glyphicon glyphicon-search"></span></div>
+                                        <div onmousedown="sploob()" class="col-xs-2"><span class="glyphicon glyphicon-user"></span></div>
+                                        <div onmousedown="sploob()" class="col-xs-2"><span class="glyphicon glyphicon-phone"></span></div>
+                                        <div onmousedown="sploob()" class="col-xs-2"><span class="glyphicon glyphicon-fire"></span></div>
+                                        <div onmousedown="sploob()" class="col-xs-2"><span class="glyphicon glyphicon-envelope"></span></div>
+                                </div>
 			</div>
 		</div>
         </div>
