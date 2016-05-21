cebeecd3c25a1e018816bfc1552d0ba27e590610
munair <munair@qe.local>
2016-05-21 14:57:15 +0900

    commenting out sidebar and adding navigation

--- base.php
+++ base.php
@@ -16,18 +16,24 @@ use Roots\Sage\Wrapper;
     <![endif]-->
     <?php
       do_action('get_header');
-      get_template_part('templates/header');
+      get_template_part('templates/navigation');
     ?>
     <div class="wrap container" role="document">
       <div class="content row">
         <main class="main">
           <?php include Wrapper\template_path(); ?>
         </main><!-- /.main -->
+
+	<!--// (combined html and php single line comment)
+	/*
         <?php if (Setup\display_sidebar()) : ?>
           <aside class="sidebar">
             <?php include Wrapper\sidebar_path(); ?>
           </aside><!-- /.sidebar -->
         <?php endif; ?>
+	*/ (closing multiple line php comment)
+	//-->
+
       </div><!-- /.content -->
     </div><!-- /.wrap -->
     <?php
