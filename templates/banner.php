3e98d41e2368e91b69567ba1968064ad7a3de477
Munair Simpson <munair@gmail.com>
2016-05-23 13:46:51 +0900

    make the whole button clickable

--- templates/banner.php
+++ templates/banner.php
@@ -12,7 +12,7 @@
 					<p> We develop <span style="font-weight: 900;">vaccines</span> for <span style="font-weight: 900;">global health</span>. </p>
 				</div>
         	        	<div class="push">
-					<div class="yellow-button"> <p> <a href="#"> join us <i class="glyphicon glyphicon-chevron-right" style="font-size:15px;"></i> </a> </p> </div>
+					<a href="#"> <div class="yellow-button"> <p> join us <i class="glyphicon glyphicon-chevron-right" style="font-size:15px;"></i> </p> </div> </a>
 				</div>
 			</div>
 		</div>
@@ -24,7 +24,7 @@
 					<p> We <span style="font-weight: 900;">partner</span> globally to fight <span style="font-weight: 900;">infectious disease</span>. </p>
 				</div>
                 		<div class="right">
-					<div class="yellow-button"> <p> <a href="#"> help us <i class="glyphicon glyphicon-chevron-right" style="font-size:15px;"></i> </a> </p> </div>
+					<a href="#"> <div class="yellow-button"> <p> help us <i class="glyphicon glyphicon-chevron-right" style="font-size:15px;"></i> </p> </div> </a>
 				</div>
 			</div>
 		</div>
@@ -36,7 +36,7 @@
 					<p> We are an <span style="font-weight: 900;">international organization</span> based in <span style="font-weight: 900;">Seoul, South Korea</span>. </p>
 				</div>
                 		<div class="up">
-					<div class="yellow-button"> <p> <a href="#"> find us <i class="glyphicon glyphicon-chevron-right" style="font-size:15px;"></i> </a> </p> </div>
+					<a href="#"> <div class="yellow-button"> <p> find us <i class="glyphicon glyphicon-chevron-right" style="font-size:15px;"></i> </p> </div> </a>
 				</div>
 			</div>
 		</div>
