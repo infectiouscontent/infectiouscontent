a0e25629cd34ca872aa8b971ce1e11ec3ae628e7
Munair Simpson <munair@gmail.com>
2016-05-24 05:01:28 +0900

    update nav with better links

--- templates/navigation.php
+++ templates/navigation.php
@@ -7,12 +7,12 @@
 		<div class="col-xs-8 col-sm-9 col-md-10">
 	        	<nav>
 		            <ul>
-		                <li><a href="#" style="text-decoration: none;"> Jobs</a></li>
-		                <li><a href="#" style="text-decoration: none;"> News</a></li>
-		                <li><a href="#" style="text-decoration: none;"> Contact Us</a></li>
-		                <li><a href="#" style="text-decoration: none;"> Support IVI</a></li>
-		                <li><a href="#contact" style="text-decoration: none;"> Sitemap</a></li>
-		                <li><a href="#" style="text-decoration: none;"> Korean</a></li>
+		                <li><a href="http://iviwebmain.ivi.int/employment/" style="text-decoration: none;"> Jobs</a></li>
+		                <li><a href="http://iviwebmain.ivi.int/?page_id=12463" style="text-decoration: none;"> News</a></li>
+		                <li><a href="#contact" style="text-decoration: none;"> Contact Us</a></li>
+		                <li><a href="/support-ivi" style="text-decoration: none;"> Support IVI</a></li>
+		                <li><a href="#footer" style="text-decoration: none;"> Sitemap</a></li>
+		                <li><a href="http://iviwebmain.ivi.int/korean/" style="text-decoration: none;"> Korean</a></li>
 		                <li><?php get_search_form(); ?></li>
 		            </ul>
 	       	 	</nav>
