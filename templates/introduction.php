811851cee86a4cd3cea6ee5b179cb88622275674
munair <munair@qe.local>
2016-07-15 12:21:08 +0900

    two sections

--- templates/introduction.php
+++ templates/introduction.php
@@ -1,30 +1,21 @@
 <section id="introduction" class="section-introduction">
 	<div class="container-fluid">
 		<div class="row">
-			<div class="col-sm-4">
+			<div class="col-sm-6">
 				<div class="featured">
-					<h2>News</h2>
+					<h2>News / Announcements</h2>
 					<div class="sections">
 						<h4>Press Releases</h4>
 						<h4>Media Coverage</h4>
 					</div>
 				</div>
 			</div>
-			<div class="col-sm-4">
+			<div class="col-sm-6">
 				<div class="featured">
 					<h2>On the blog</h2>
 					<div class="sections">
 						<h4>Vaccinology</h4>
 						<h4>Symposia</h4>
-					</div>
-				</div>
-			</div>
-			<div class="col-sm-4">
-				<div class="featured">
-					<h2>Announcements</h2>
-					<div class="sections">
-						<h4>Institutional</h4>
-						<h4>Bids</h4>
 					</div>
 				</div>
 			</div>
