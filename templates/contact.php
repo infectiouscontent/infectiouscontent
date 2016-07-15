3bcce2fbb0a98138c5bc47fec47f3b08e822d517
munair <munair@qe.local>
2016-07-15 12:31:35 +0900

    minimalize contact

--- templates/contact.php
+++ templates/contact.php
@@ -3,10 +3,8 @@
 			<div class="row">
 				<div class="title">
 					<h1>Contact Us</h1>
-					<p>The Internation Vaccine Institute is based in Seoul, Korea. To visit our offices please refer to the information below:</p>
 				</div>
 				<a href="#ribbon"><img class="scroll-down" src="https://s3.ap-northeast-2.amazonaws.com/international-vaccine-institute/ivi-icon-1450x1450px.png" /></a>
-				<h1>IVI</h1>
 				<p>International Vaccine Institute</p>
 				<p class="address">SNU Research Park, 1 Gwanak-ro, Gwanak-gu, Seoul, 151-742 Korea TEL:02-872-2801, FAX:02-872-2803 </p>
 			</div>
